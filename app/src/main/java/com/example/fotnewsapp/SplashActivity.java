e2b8ca42f7737069475b4ee96cb35ce654af0afa
SeshanDev <seshan.nethmika@gmail.com>
2025-05-31 01:37:20 +0530

    fourth commit

---
+++ app/src/main/java/com/example/fotnewsapp/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.fotnewsapp;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Navigate to SignupActivity or MainActivity after delay or on some logic
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, SignupActivity.class));
+            finish();
+        }, 1000); // 2 seconds splash
+    }
+}
+
